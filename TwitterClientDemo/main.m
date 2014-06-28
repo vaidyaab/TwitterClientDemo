eb19f610788c8986c858c7a18bbe75c3e1ff0b32
abhijetv <abhijetv@yahoo-inc.com>
2014-06-28 15:21:52 -0700

    Initial Commit

---
+++ TwitterClientDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TwitterClientDemo
+//
+//  Created by Abhijeet Vaidya on 6/28/14.
+//  Copyright (c) 2014 yahoo inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
