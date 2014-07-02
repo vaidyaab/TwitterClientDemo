4dbe30cf25571b3b1286e9ac58838221dd4a5bb1
abhijetv <abhijetv@yahoo-inc.com>
2014-07-02 01:16:57 -0700

    added char counting and navigation for tweet compose

--- TwitterClientDemo/ComposeTweetViewController.m
+++ TwitterClientDemo/ComposeTweetViewController.m
@@ -16,6 +16,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *authorHandleLabel;
 @property (weak, nonatomic) IBOutlet UITextView *tweetText;
 
+@property (strong,nonatomic) UILabel *charCount;
+
 @end
 
 @implementation ComposeTweetViewController
@@ -38,7 +40,46 @@
     self.authorHandleLabel.text = [user screenName];
     self.authorNameLabel.text = [user name];
     [self.authorImageView setImageWithURL:[NSURL URLWithString:[user profileImageUrl]]];
+    
+    self.tweetText.delegate = self;
+    
+    self.charCount = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 30, 12)];
+    self.charCount.text = @"140";
+    self.charCount.textColor = [UIColor grayColor];
+    
+    
+    UIBarButtonItem *charCounterButton = [[UIBarButtonItem alloc] initWithCustomView:self.charCount];
+    
+    UIBarButtonItem *cancelButton = [[UIBarButtonItem alloc] initWithTitle:@"Cancel" style:UIBarButtonItemStylePlain target:self action:@selector(onCancel:)];
+    
+    
+    self.navigationItem.rightBarButtonItems = [NSArray arrayWithObjects:cancelButton, charCounterButton, nil];
 
+}
+
+-(IBAction)onCancel:(id)sender {
+    NSLog(@"cancel clicked.");
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (void)textViewDidChange:(UITextView *)textView {
+    
+    
+    self.charCount.text = [NSString stringWithFormat:@"%d",140 - [[NSString stringWithFormat:@"%d",[textView.text length]] intValue]];
+}
+         
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text{
+             
+             
+    if([text isEqualToString:@"\b"]){
+        
+        return YES;
+        
+    }else if([[textView text] length] - range.length + text.length > 140){
+        
+        return NO;
+    }
+    return YES;
 }
 
 - (void)didReceiveMemoryWarning
